2abca55ce530b02f811c348da68a47f6834fc51f
siphosenkosi803 <110998052+siphosenkosi803@users.noreply.github.com>
2023-06-27 02:15:37 +0200

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
+    const task = true;
     const task2 = false;
     return [task, task2];
   }
