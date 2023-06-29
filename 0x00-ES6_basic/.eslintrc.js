73077234fa59785b6f1834a66cd1ea3cd9932d74
siphosenkosi803 <sibusisoradebe943@yahoo.com>
2023-06-29 01:10:07 +0000

    updated config files

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,7 +1,35 @@
 module.exports = {
-  extends: 'eslint:recommended',
-  rules: {
-    // Add your ESLint rules here
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
   },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
-
