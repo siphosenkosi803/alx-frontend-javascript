7edcc4fae783e32cb8ba3a8bc72c729d35075aea
siphosenkosi803 <sibusisoradebe943@yahoo.com>
2023-06-29 01:38:53 +0000

    update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    {
-      var task = true;
-      var task2 = false;
-    }
+    return [task, task2];
   }
-
   return [task, task2];
 }
