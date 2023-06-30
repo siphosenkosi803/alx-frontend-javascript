e2491f0e57246d185131b6a2bd4bf457ff2f7822
siphosenkosi803 <sibusisoradebe943@yahoo.com>
2023-06-30 12:58:45 +0000

    0-constant.js fix

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+export default function createIteratorObject(report) {
+    const emps = [];
+      for (const dep of Object.keys(report.allEmployees)) {
+          for (const emp of report.allEmployees[dep]) {
+        emps.push(emp);
+      }
+    }
+    return emps;
+  }
